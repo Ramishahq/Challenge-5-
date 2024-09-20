57c6558df817166e2200da90d975a687c5c44e23
ramishahq <hoque2@usf.edu>
2024-09-20 00:23:42 -0400

    Create placeOrder function

--- Customer Order Management System.js
+++ Customer Order Management System.js
@@ -15,32 +15,56 @@ let inventory = [
  let customerOrders = []
  
 // Task 3: Create a Function to Place an Order
+
 function placeOrder( customerName,orderItems) {
+
 // used`for…of-loop to iterates over iterable objects like arrays, strings, etc.)
+// checking if all ordered item are in stock or not.
 
    for (let orderedItem of orderItems) {
-    // now to find the product in the inventory using find formula
 
-let product = inventory.find (item=> item.name === orderedItem.name);
+let item = inventory.find (item=> item.name === orderedItem.name);
 
 // Next to check if we have the product or we have enough in stock
-if (product.quantity>= orderedItem.quantity)
-    {
+
+        if (item === null) {
+            console.log(`Error: ${orderedItem.name} Item is not found in inventory.`);
+            return; 
+            // It will Stop processing if item is not found
+        }
+     // If the the item is less or out of stock 
+
+     if (item.quantity < orderedItem.quantity) {
+        console.log(`Error: ${orderedItem.name} is out of stock or not enough quantity available.`);
+        return; // Stop processing if not enough stock
+    }
+}
+// If items are available we will update the stock
  //we will have to deduct the ordered quantity from the inventory stock
-    product.quantity -= orderedItem.quantity;
-    }else { 
-    console.log(`Error: ${orderedItem.name} is out of stock.`);
+ for (let orderedItem of orderItems) {
+    let item = inventory.find(item => item.name === orderedItem.name);
+ item.quantity -= orderedItem.quantity;
+   
 }
-}
+// Check inventory 
+console.log(inventory);
+// output
+//name: 'Espresso', price: 3, quantity: 10}
+//name: 'Latte', price: 4, quantity: 2}
+//name: 'Cappuccino', price: 4, quantity: 6}
+//name: 'Mocha', price: 5, quantity: 4}
+
 // if all items are found we will add order by using push method in the ordered array.
 
 customerOrders.push(
     { customerName: customerName,
         items: orderItems,
-        status: " Pending "}
+        status: " Pending "
+    }
 );
-
+console.log (`Order was placed succesfully for customerName ${customerName} `);
 }
+
 // testing the placeOder funstion with an example order
 let customerName1 = 'Raiyan Zakir'
 let orderedItems1 = [
@@ -55,8 +79,11 @@ let orderedItems2 = [
     
 ];
 placeOrder(customerName1, orderedItems1);
+// output Order was placed succesfully for customer Raiyan Zakir 
+
 
 placeOrder(customerName2, orderedItems2);
+// output Error: Mocha is out of stock or not enough quantity available
 
 console.log(customerOrders);
 // output customerName1 "Raiyan Zakir"
@@ -67,6 +94,8 @@ console.log(customerOrders);
 //items {name: 'Mocha', quantity: 10}
 //status " Pending "
 // output Error: Mocha is out of stock.
+
+
 
 
 
